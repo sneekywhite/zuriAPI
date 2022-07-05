4bccbf1bcf6a14b97e9ff5e5a3640ed3d3cb9606
sneekywhite <okoh.onyeka123@gmail.com>
2022-07-05 17:06:33 +0100

    DRF generics changed in views.py

--- I4G004667BL2/links/views.py
+++ I4G004667BL2/links/views.py
@@ -3,26 +3,26 @@ from django.shortcuts import render
 from links.serializer import LinkSerializer
 from .models import link
 from .serializer import LinkSerializer
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView,UpdateAPIView, DestroyAPIView
 
 # Create your views here.
 class PostListApi(ListAPIView):
     queryset = link.objects.filter(active = True)
     serializer_class = LinkSerializer
 
-class PostDetailApi(ListAPIView):
+class PostDetailApi(RetrieveAPIView):
     queryset = link.objects.filter(active = True)
     serializer_class = LinkSerializer
 
-class PostUpdateApi(ListAPIView):
+class PostUpdateApi(UpdateAPIView):
     queryset = link.objects.filter(active = True)
     serializer_class = LinkSerializer
 
-class PostDeleteApi(ListAPIView):
+class PostDeleteApi(DestroyAPIView):
     queryset = link.objects.filter(active = True)
     serializer_class = LinkSerializer
 
-class PostCreateApi(ListAPIView):
+class PostCreateApi(CreateAPIView):
     queryset = link.objects.filter(active = True)
     serializer_class = LinkSerializer
 
